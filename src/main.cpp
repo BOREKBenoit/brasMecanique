fd1e38bb5fd1f703b74d1e43f71d5b70aae99bca
BOREKBenoit <borekbenoit8@gmail.com>
2024-11-28 14:00:14 +0100

    Premier programme de test

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,41 @@
-#include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+/*
+  simpleMovements.ino
+
+ This  sketch simpleMovements shows how they move each servo motor of Braccio
+
+ Created on 18 Nov 2015
+ by Andrea Martino
+
+ This example is in the public domain.
+ */
+
+#include <Braccio.h>
+#include <Servo.h>
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  //Initialization functions and set up the initial position for Braccio
+  //All the servo motors will be positioned in the "safety" position:
+  //Base (M1):90 degrees
+  //Shoulder (M2): 45 degrees
+  //Elbow (M3): 180 degrees
+  //Wrist vertical (M4): 180 degrees
+  //Wrist rotation (M5): 90 degrees
+  //gripper (M6): 10 degrees
+  Braccio.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Braccio.ServoMovement(20,           90,  90, 90, 90, 90, 73);  
+
+
+
 }
