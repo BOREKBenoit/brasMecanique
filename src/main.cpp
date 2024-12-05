93b47887598b6e61ffbfc0b7082f9a77d4a7adcf
BOREKBenoit <borekbenoit8@gmail.com>
2024-12-05 16:17:54 +0100

    modification

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@
  
 #include <Braccio.h>
 #include <Servo.h>
-#include <SPI.h>
+
 
 Servo base;
 Servo shoulder;
@@ -26,44 +26,33 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
-int objects = 2; // Cette variable va servir pour dire au robot dans qu'elle position il devra placer l'objet
-volatile byte receivedData;
+int time = 5;
 
 void setup() {
-  SPI.begin(); // Initialisation en tant qu'esclave
-  SPI.setClockDivider(SPI_CLOCK_DIV8); // Réglage de la vitesse du SPI
-  pinMode(10, INPUT); // SS comme entrée
-  SPI.attachInterrupt(); // Activer les interruptions SPI
-  Serial.begin(9600);
 
+  Serial.begin(9600);
 
 Braccio.begin();
 Serial.println("Start");
 }
 
-ISR(SPI_STC_vect) { // Interruption lorsqu'une donnée est reçue
-  receivedData = SPDR; // SPDR contient les données reçues
-}
+
+
 
 void loop() {
- if (receivedData) {
-    Serial.print("Reçu : ");
-    Serial.println((char)receivedData);
-       if((char)receivedData == 'H'){
-      Braccio.ServoMovement(30, 90, 90, 180, 180, 90, 10);
 
-    }
+      Braccio.ServoMovement(time, 90, 90, 180, 180, 90, 10);
 
-    receivedData = 0; // Réinitialiser après affichage
+      Braccio.ServoMovement(time, 90, 90, 180, 180, 90, 10);
+      delay(50000);
 
- 
-  }
-
-//Placement du robot en position de capture
-//Braccio.ServoMovement(30, 90, 90, 180, 180, 90, 10);
+      
+      
+      
 
 
-delay(5000);
+
+
 
 
 
