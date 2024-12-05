74324d740734d8be209beb2f634447df69d24070
CapBrindille <mathis.benoit@hotmail.com>
2024-12-05 17:03:05 +0100

    création du master

---
+++ src/main_master.cpp
@@ -0,0 +1,31 @@
+/*
+    Voici le programme pour la carte master qui va envoyer des caractères à l'aide du protocole SPI à la carte slave 
+    qui va contrôler le bras
+
+    Programme basique sur Arduino UNO
+  Compilé sur Visual Studio Code v1.95.2
+  Avec l'IDE PlatformIO v3.3.3
+  
+  Constituants :
+  -  Bras mécanique Braccio
+  -  Adruino UNO avec shield associé
+
+  Version 1 : 05/12/2024
+
+  -  Benoit BOREK
+  -  Mathis BENOIT
+  -   Alban de Farcy de Pontfarcy
+*/
+
+#include <Arduino.h>
+#include <Braccio.h>
+#include <SPI.h>
+#include <Servo.h>
+
+void setup(){
+
+}
+
+void loop(){
+    
+}
